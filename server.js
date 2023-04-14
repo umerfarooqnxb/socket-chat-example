515d6a25000a2b594c04395d37e70e943ed2c58d
umerfarooqnxb <umer.farooq2885@nxb.com.pk>
2023-04-14 19:30:26 +0500

    Commit

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const http = require("http");
-const server = http.createServer(app);
 const app = express();
+const server = http.createServer(app);
 
 app.listen(5001, () => console.log("Server is listening on the port 5001"));
