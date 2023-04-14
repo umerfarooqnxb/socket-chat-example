b522086c65afc4c00cc775a3005b35812ed61f46
umerfarooqnxb <umer.farooq2885@nxb.com.pk>
2023-04-14 19:44:48 +0500

    App is listening on port 5001

--- server.js
+++ server.js
@@ -3,4 +3,10 @@ const http = require("http");
 const app = express();
 const server = http.createServer(app);
 
+// Rest API's
+
+app.get('/', (req, res) => {
+    res.send('<h1>Hello world</h1>');
+  });
+
 app.listen(5001, () => console.log("Server is listening on the port 5001"));
