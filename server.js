30b25f6802529c2558d9e54b3f90b4853330c5f1
umerfarooqnxb <umer.farooq2885@nxb.com.pk>
2023-04-14 20:15:46 +0500

    Integrating Socket IO

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const http = require("http");
 const app = express();
 const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
 
 // Rest API's
 
@@ -9,4 +11,8 @@ app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
 
-app.listen(5001, () => console.log("Server is listening on the port 5001"));
+io.on("connection", (socket) => {
+  console.log("a user connected");
+});
+
+server.listen(5001, () => console.log("Server is listening on the port 5001"));
