d12b97297dad29debc9cc26644dac3fc6eec4898
umerfarooqnxb <umer.farooq2885@nxb.com.pk>
2023-04-14 19:20:40 +0500

    Revert "first commit"
    This reverts commit 997235a6bab02f1ae514818d7cf5b15886904eb3.

--- server.js
+++
@@ -1,6 +0,0 @@
-const express = require("express");
-const http = require("http");
-const server = http.createServer(app);
-const app = express();
-
-app.listen(5001, () => console.log("Server is listening on the port 5001"));
